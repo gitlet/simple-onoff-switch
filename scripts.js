870d608d24887e53851fd7cff125c58f222d289f
gitlet <11thdim@gmail.com>
2016-09-02 04:49:10 +0400

    First commit, uploading files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.container .caption-and-btn .btn').click(function() {
+    $('.tia').toggleClass('move');
+  $('.container .tia').click(function() {
+    $('.tia').toggleClass('move');
+  });
+  });
+});
